51916946a4019ca99782cd411205232e9e65d892
monthebrice2000 <monthedjeumoubrice2000@gmail.com>
2021-12-09 16:26:20 +0100

    Ajout de l'instruction pour afficher le message en cas d'interruption du thread v4 ( mise à jour )

--- src/prodcons/v4/ProdConsBuffer.java
+++ src/prodcons/v4/ProdConsBuffer.java
@@ -1,12 +1,10 @@
 package prodcons.v4;
 
 
-import com.sun.source.doctree.ThrowsTree;
 import prodcons.IProdConsBuffer;
 import prodcons.Message;
 import prodcons.XMLParameters;
 
-import java.util.concurrent.Semaphore;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
@@ -27,10 +25,9 @@ public class ProdConsBuffer implements IProdConsBuffer{
     private Message[] buffer = new Message[bufSz];
     private int notEmpty = 0;
     private int notFull = bufSz;
-    private Lock mutex_prod_lock = new ReentrantLock();
-    private Condition condition_prod_lock = mutex_prod_lock.newCondition();
-    private Lock mutex_cons_lock = new ReentrantLock( );
-    private Condition condition_cons_lock = mutex_prod_lock.newCondition();
+    private Lock mutex = new ReentrantLock();
+    private Condition condition_prod_lock = mutex.newCondition();
+    private Condition condition_cons_lock = mutex.newCondition();
     private int in = 0;
     private int out = 0;
     
@@ -41,27 +38,14 @@ public class ProdConsBuffer implements IProdConsBuffer{
     //Nonbre de threads ayant terminé la consommation d'un message
     private int nbConsDone = 0;
     private int nbProdDone = 0;
-    private int waitings_prods = 0;
-    private int waitings_cons = 0;
     
     @Override
     public void put(Message m) throws InterruptedException {
-        mutex_prod_lock.lock();
-        try {
-            while( nbConsDone<nbCons && notFull == 0 ){
+        mutex.lock();
 
-                System.out.println("Thread producteur en attente");
-                if ( waitings_cons > 0 ){
-                    waitings_prods ++;
-                    condition_cons_lock.signalAll();
-
-                }
-                mutex_prod_lock.unlock();
-                condition_prod_lock.wait();
-                mutex_prod_lock.lock();
-                System.out.println("l s'est reveillé");
-                waitings_prods--;
-
+        try{
+            if ( nbConsDone<nbCons && notFull == 0 ){
+                condition_prod_lock.await();
             }
 
             if(nbConsDone == nbCons) {
@@ -82,13 +66,13 @@ public class ProdConsBuffer implements IProdConsBuffer{
 
 
             condition_cons_lock.signalAll();
-
         }catch ( InterruptedException exc ){
-            throw new InterruptedException( "le thread est interrompu ");
+            throw new InterruptedException( exc.getMessage() );
         }finally {
-            System.out.println("Producteur quitté");
-            mutex_prod_lock.unlock();
+            mutex.unlock();
         }
+
+
 
 
     }
@@ -101,23 +85,19 @@ public class ProdConsBuffer implements IProdConsBuffer{
 
     @Override
     public Message get() throws InterruptedException {
-    	mutex_prod_lock.lock();
+    	mutex.lock();
     	Message m = null;
-    	try {
-    	    while ( notEmpty == 0 ){
 
-                System.out.println("Thread consommateur en attente+++");
-                if ( waitings_prods > 0 ){
-                    condition_prod_lock.signalAll();
-                    waitings_cons ++;
+    	try{
+            if ( notEmpty == 0 ){
+                condition_cons_lock.await();
+            }
 
-                }
-                mutex_prod_lock.unlock();
-                condition_cons_lock.wait();
-                mutex_prod_lock.lock();
-                System.out.println("l s'est reveillé");
-                waitings_cons--;
-
+            if( nmsg() <= 0) {
+                condition_cons_lock.signalAll();
+                //mutex_prod_lock.unlock();
+                Thread.currentThread().interrupt();
+                throw new InterruptedException( "Un consommateur est interrompu par inssufisance de producteur");
             }
 
             m = buffer[out];
@@ -128,16 +108,17 @@ public class ProdConsBuffer implements IProdConsBuffer{
             nbConsDone++;
             notFull++;
 
+
             System.out.println("Un message consommé, messages disponibles : " + nmsg() + ", total produit : "+totmsg);
 
             condition_prod_lock.signalAll();
-
         }catch ( InterruptedException exc ){
-    	    exc.getMessage();
+    	    throw new InterruptedException( exc.getMessage() );
         }finally {
-    	    System.out.println("Consommateur quitté");
-            mutex_prod_lock.unlock();
+            mutex.unlock();
         }
+
+
 
         return m;
     }
